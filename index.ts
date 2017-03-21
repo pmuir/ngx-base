858050ea46dfabf7b53513ae0398d40217089ef2
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-21 17:16:14 -0400

    feat: add config and val wrapper

--- index.ts
+++ index.ts
@@ -9,3 +9,9 @@ export { Broadcaster } from './src/app/broadcaster.service';
 
 // Logger
 export { Logger } from './src/app/logger.service';
+
+// Config
+export { ConfigStore } from './src/app/config.store';
+
+// ValWrapper
+export { ValWrapper } from './src/app/val-wrapper';
